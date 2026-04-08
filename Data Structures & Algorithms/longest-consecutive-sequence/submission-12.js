435ec70085a78fd1341e766da9077a3512a70519
tadhg-myler <tadhg.myler@gmail.com>
2026-04-08 15:19:17 +0100

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let longestSeq = [],
+            counter = 1;
+
+        if (nums.length === 0) return 0;
+        if (nums.length === 1) return 1;
+
+        console.log( nums.sort((a, b) => a - b))
+        nums.sort((a, b) => a - b).forEach((n, idx) => {
+            if (n + 1 === nums[idx + 1]) {
+                counter++;
+            } else if (n !== nums[idx + 1]) {
+                longestSeq.push(counter)
+                counter = 1;
+            }
+        })
+
+        console.log( longestSeq)
+        return longestSeq.sort((a, b) => a - b).pop()
+    }
+}
